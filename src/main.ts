4f5e5d74e9d78d555f5eea584271468ff6fbbb33
cwuyiqing <cwuyiqing@gmail.com>
2021-05-21 17:40:26 +0800

    fix: fix docker port

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module'
 
 const expressApp = express()
 const adapter = new ExpressAdapter(expressApp)
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5000
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule, adapter, {
